3e807123dd8545dcd8917ef426586c05876a8600
Dobrosław Żybort <matrixik@gmail.com>
2013-02-17 02:56:18 +0100

    Init

---
+++ README.md
@@ -0,0 +1,24 @@
+rand
+====
+
+Package rand simplify chosing random values.
+
+[Documentation online](http://godoc.org/bitbucket.org/gosimple/rand)
+
+	nr := rand.IntFT(10, 20)	// 10 <= nr <= 20
+	bt := rand.IntBT(10, 20)	// 10 < bt < 20
+
+### Requests or bugs? 
+<https://bitbucket.org/gosimple/rand/issues>
+
+## Installation
+
+	go get -u bitbucket.org/gosimple/rand
+
+## License
+
+The source files are distributed under the 
+[Mozilla Public License, version 2.0](http://mozilla.org/MPL/2.0/),
+unless otherwise noted.  
+Please read the [FAQ](http://www.mozilla.org/MPL/2.0/FAQ.html)
+if you have further questions regarding the license.
---
+++ rand.go
@@ -0,0 +1,59 @@
+// Copyright 2013 by Dobrosław Żybort. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+// Choosing randomness simplified
+package rand
+
+import (
+	"math/rand"
+	"time"
+)
+
+//=============================================================================
+
+func init() {
+	rand.Seed(time.Now().UTC().UnixNano())
+}
+
+// IntFT returns a random number from min to max (0 <= min <= nr <= max)
+// Will return 0 if max < 0
+func IntFT(min, max int) int {
+	var choice int
+
+	if max < 0 {
+		return 0
+	}
+	if min < 0 {
+		min = 0
+	}
+
+	if min < max {
+		choice = min + rand.Intn(max-min+1)
+	} else {
+		choice = min
+	}
+	return choice
+}
+
+// IntBT returns a random number between min to max (min < nr < max)
+// Will return 0 if max < 0
+func IntBT(min, max int) int {
+	min += 1
+	var choice int
+
+	if max < 0 {
+		return 0
+	}
+	if min < 0 {
+		min = 0
+	}
+
+	if min < max {
+		choice = min + rand.Intn(max-min)
+	} else {
+		choice = min
+	}
+	return choice
+}
