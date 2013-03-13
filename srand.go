201ea4aae27de1f52f26fcb3adda9895e5c03f49
Dobrosław Żybort <matrixik@gmail.com>
2013-03-13 15:21:10 +0100

    Update doc

--- srand.go
+++ srand.go
@@ -3,7 +3,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-// Package srand implements simple pseudo-random generators.
+// Package srand simplify choosing random values, rand helper.
 package srand
 
 import (
