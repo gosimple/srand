5df5e19be4d57b49c65776982fef98c5e31fee38
Dobrosław Żybort <matrixik@gmail.com>
2013-05-10 20:25:57 +0200

    Breaking change: rename all IntFT to Int

--- srand.go
+++ srand.go
@@ -17,10 +17,10 @@ func init() {
 	rand.Seed(time.Now().UTC().UnixNano())
 }
 
-// IntFT returns a non-negative random number from min to max
+// Int returns a non-negative random number from min to max
 // (0 <= min <= nr <= max).
 // Will return 0 if max < 0
-func IntFT(min, max int) int {
+func Int(min, max int) int {
 	var choice int
 
 	if max < 0 {
@@ -38,22 +38,22 @@ func IntFT(min, max int) int {
 	return choice
 }
 
-//func IntFTChoice(min, max int) int {
-//	return IntFT(min, max int)
+//func IntChoice(min, max int) int {
+//	return Int(min, max int)
 //}
 
-// IntFTMany returns a slice of non-negative random numbers from min to max.
-func IntFTMany(min, max, quantity int) (out []int) {
+// IntMany returns a slice of non-negative random numbers from min to max.
+func IntMany(min, max, quantity int) (out []int) {
 	for i := 0; i < quantity; i++ {
 		out = append(out, IntFT(min, max))
 	}
 	return out
 }
 
-// IntFTSample returns a slice of non-negative random, unique numbers
+// IntSample returns a slice of non-negative random, unique numbers
 // from min to max. If the quantity is bigger than max - min sample size
 // will be the size of max - min.
-//func IntFTSample(min, max, quantity int) (sample []int) {
+//func IntSample(min, max, quantity int) (sample []int) {
 //	for i := 0; i < quantity; i++ {
 //		sample = append(sample, IntFT(min, max))
 //	}
